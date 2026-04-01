924f439634f7028ebe772332d2863bbae886cebf
Kishan koundinya <kishankoundinya31@gmail.com>
2026-04-02 01:39:22 +0530

    deployment fix

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { userAuth } = require('../middleware/userAuth');
+const { userAuth } = require('../Middleware/userAuth');
 const { userModel } = require('../model/userModel');
 
 // Place order endpoint
